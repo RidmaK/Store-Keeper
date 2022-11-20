1bf3cf577f5011296901e314584590d02b954797
ridma <ridma@ceylonlinux.lk>
2022-11-20 22:26:00 +0530

    new

--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -104,7 +104,7 @@
           </a>
         </li>
         @endcan
-        <li class="nav-item">
+        {{-- <li class="nav-item">
           <a class="nav-link text-white " href="../pages/billing.html">
             <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
               <i class="material-icons opacity-10">receipt_long</i>
@@ -146,7 +146,7 @@
             </div>
             <span class="nav-link-text ms-1">Sign In</span>
           </a>
-        </li>
+        </li> --}}
       </ul>
     </div>
     <div class="sidenav-footer position-absolute w-100 bottom-0 ">
